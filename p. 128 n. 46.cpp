7f26daa4d1cb1fa482d1c217f9044e891e4f059f
catepava <catepava@gmail.com>
2016-11-05 11:57:19 +0100

    esercizio per casa
    non finito

---
+++ p. 128 n. 46.cpp
@@ -0,0 +1,14 @@
+//p. 128 n. 46
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    float angolo;
+    cout<<"Inserisci il valore dell'angolo: /n";
+    cin>> angolo;
+
+
+}
