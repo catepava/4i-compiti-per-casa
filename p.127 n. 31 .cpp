dad8d07fe8b0cd10654cf592f1e8a5b5d9ca5b82
catepava <catepava@gmail.com>
2016-11-07 11:26:46 +0100

    compiti per casa

---
+++ p.127 n. 31 .cpp
@@ -0,0 +1,20 @@
+// p. 127 n. 31
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int i=0;
+    string frase;
+    cout<<"Qnserire la stringa da ripetere (al posto degli spazi inserire - ): " <<endl;
+    cin>> frase;
+    cout<<"Questa e' la stringa inserite ripetuta per tre volte: "<<endl;
+
+    for(i=0; i<3; i++)
+        {
+        cout<< frase <<endl;
+        }
+
+}
