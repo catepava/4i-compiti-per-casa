78751a92f1932784eda1c27a67b160bcf213a120
catepava <catepava@gmail.com>
2016-11-07 11:32:56 +0100

    compiti per casa

---
+++ p.127 n. 32 .cpp
@@ -0,0 +1,15 @@
+//p.127 n.32
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int lato=0 , area=0, perimetro=0 ;
+cout<<"Inserire valore lato quadrato: " <<endl;
+cin>> lato;
+area= lato*lato;
+perimetro= lato*4;
+
+cout<<"L'area del quadrato vale: "<<area<< endl << "Il perimetro del quadrato vale: "<<perimetro<< endl;
+}
